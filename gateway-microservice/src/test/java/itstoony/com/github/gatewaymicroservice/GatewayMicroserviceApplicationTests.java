5aa51b7e7431999fb12281a212582f302b4756bb
Tony <toonyrenner@gmail.com>
2023-06-04 18:01:04 -0300

    Setting up microservices arch

---
+++ gateway-microservice/src/test/java/itstoony/com/github/gatewaymicroservice/GatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package itstoony.com.github.gatewaymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
